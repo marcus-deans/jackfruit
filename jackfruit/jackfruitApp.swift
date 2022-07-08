ad7f6813872c240e4e7d2ad947bcf12ada92d101
Marcus Deans <marcus.deans@duke.edu>
2022-07-07 20:52:55 -0700

    Initial Commit

---
+++ jackfruit/jackfruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  jackfruitApp.swift
+//  jackfruit
+//
+//  Created by Marcus Deans on 2022-07-07.
+//
+
+import SwiftUI
+
+@main
+struct jackfruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
