2139ab74afd0e6cdc417c2fa95fcf4371e300e10
Marcus Deans <marcus.deans@duke.edu>
2022-07-20 02:10:54 -0700

    Screen6EmailPreview

--- jackfruit/onboarding/Screen6EmailView.swift
+++ jackfruit/onboarding/Screen6EmailView.swift
@@ -47,61 +47,9 @@ final class Screen6EmailVM: ObservableObject, Completeable {
 
 struct Screen6EmailView: View {
     @StateObject var vm: Screen6EmailVM
-    @State private var editing = false
-    @State var progressValue: Float = 0.64
-    @State private var keyboardHeight: CGFloat = 0
     
     var body: some View {
-        ZStack {
-            Color.init(UIColor.transitionPage)
-                .ignoresSafeArea()
-            
-            GeometryReader { _ in
-                VStack(alignment: .leading) {
-                    
-                    ProgressBar(value: $progressValue).frame(height: 10)
-                    
-                    Text("What's your email?")
-                        .font(Font.custom("CircularStd-Black", size: 40))
-                        .padding(.bottom, 20)
-                    
-                    Text("When you share your contact this will automatically be shared!")
-                        .font(Font.custom("CircularStd-Book", size: 20))
-                        .padding(.bottom, 40)
-                    
-                    
-                    TextField("Email", text: $vm.emailAddress, onEditingChanged: { edit in
-                        self.editing = edit
-                    }).padding(.bottom, 200)
-                        .textFieldStyle(MyTextFieldStyle(focused: $editing)).font(Font.custom("CircularStd-Book", size: 22))
-                        .textContentType(.emailAddress)
-                        .keyboardType(.emailAddress)
-                        .textInputAutocapitalization(.never)
-                    
-                }.padding(EdgeInsets(top: 0, leading: 50, bottom: 0, trailing: 50))
-                    .fixedSize(horizontal: false, vertical: true)
-                    .background(Color.init(UIColor.transitionPage))
-            }
-            VStack (alignment: .trailing) {
-                Spacer()
-                    .frame(minHeight: 15, idealHeight: 52, maxHeight: .infinity)
-                Button(action: {
-                    self.vm.didTapNext()
-                }, label: { Text(">") })
-                
-                
-                .padding(.leading, 250)
-                .padding(.bottom, 40)
-                .disabled(!vm.isValid)
-                .buttonStyle(BlueButtonStyle())
-            }
-        }.ignoresSafeArea(.keyboard)
+        Screen6EmailPure(didTapNextAction: vm.didTapNext, emailAddress: $vm.emailAddress, isValid: vm.isValid)
     }
 }
 
-
-struct Screen6EmailView_Previews : PreviewProvider {
-    static var previews: some View {
-        Screen6EmailView(vm: Screen6EmailVM(email: "marcusddeans@outlook.com"))
-    }
-}
